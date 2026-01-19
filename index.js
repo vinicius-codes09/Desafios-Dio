50ced48d195d669deff10244b4457d138608115b
vinicius-codes09 <vinicius09galioto@gmail.com>
2026-01-18 22:44:26 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Oi");
+let heroi= "Homem-Aranha";
+let xp=7500;
+let nivel;
+switch (true) {
+      case xp<1000:
+        nivel = "Ferro";
+        break;
+      case xp>= 1001 && xp<=2000:
+        nivel = "Bronze";
+        break;
+      case xp>= 2001 && xp<=5000:
+        nivel = "Prata";
+        break;
+      case xp>=5001 && xp<=7000:
+        nivel = "Ouro";
+        break;
+      case xp>=7001 && xp<=8000:
+        nivel = "Platina";
+        break;
+      case xp>=8001 && xp<=9000:
+        nivel = "Ascendente";
+        break;
+      case xp>=9001 && xp<=10000:
+        nivel = "Imortal";
+        break;
+      case xp>10001:
+        nivel = "Radiante";
+        break;
+
+      default:
+      nivel = "Sem nível";
+}
+
+console.log("O Herói de nome " + heroi + " está no nível de: " + nivel);
